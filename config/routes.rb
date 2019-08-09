0a51fff37b6cf5358653c0ff40d625b51341e723
Andy Weiss <andyweiss1982@gmail.com>
2019-08-09 15:40:33 -0400

    clean up functionality and code

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'profile' => 'profile#index'
-  post 'profile/:id/edit' => 'profile#edit'
-  patch 'profile' => 'profile#update'
-  
   get 'welcome/index'
   root 'welcome#index'
 
@@ -14,7 +10,5 @@ Rails.application.routes.draw do
     resources :recipes, only: [:index]
   end
   
-  resources :profile, only: [:index, :show, :edit, :update]
-
-  
+  resources :profiles, only: [:show, :edit, :update]
 end
