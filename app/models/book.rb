5711a6ce6efaa7a23ac6b159b10475561362af5f
Andy Weiss <andyweiss1982@gmail.com>
2019-08-09 16:05:08 -0400

    move pdf gen to model, can see from show page

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,42 +6,66 @@ class Book < ApplicationRecord
 
   require "open-uri"
   include Rails.application.routes.url_helpers
+
+  after_create :make_pdf
+
+  def make_pdf
+    Prawn::Document.generate(
+      "cookbook#{id}.pdf",
+      page_size: [450, 666],
+      page_layout: :portrait
+    ) do |pdf|
+      # replace this with the actual book's recipes once you have a UI to add recipes to a book
+      pdf.move_down 200
+
+      pdf.font "Helvetica", style: :bold
+      pdf.font_size(50) {
+        pdf.text name, align: :center
+      }
+      pdf.stroke { pdf.line [200, 200], [300, 150] }
+    
+      pdf.move_down 10
+      pdf.text "Meals with XXX", :align => :center
+    
+      pdf.start_new_page
+
+      Recipe.all.each do |recipe|
+        pdf.move_down 80
+        pdf.font "Helvetica", style: :bold
+    
+        pdf.font "Helvetica", style: :normal
+        pdf.text "By: #{recipe.user.email}" , :align => :right
+        pdf.move_down 90
+    
+        pdf.font "Helvetica", style: :bold
+        pdf.font_size(30) {
+        pdf.text recipe.name ,
+        :align => :center}
+        pdf.move_down 70
+    
+        pdf.font "Helvetica", style: :normal
+        pdf.font_size(22) {
+        pdf.text "Ingredients:"   ,:align => :center}
+        pdf.font "Helvetica", style: :normal
+        pdf.text recipe.ingredients ,
+        :align => :center
+    
+        pdf.move_down 70
+        pdf.font "Helvetica", style: :normal
+        pdf.font_size(22) {
+        pdf.text "Instructions:"   ,:align => :center}
+
+        pdf.font "Helvetica", style: :normal
+        pdf.text recipe.instructions, :align => :center
+        pdf.start_new_page
+
+        # image_file = open(url_for(recipe.image))
+        # pdf.image image_file.path, width: 350,  :align => :center
+        pdf.start_new_page
+      end
+    end
+  end
 end
-#   after_create :make_pdf
-
-#   def make_pdf
-
-# Prawn::Document.generate(
-#   "cookbook1.pdf",
-#   page_size: [450, 666],
-#   page_layout: :portrait
-# ) do |pdf|
-#   # replace this with the actual book's recipes once you have a UI to add recipes to a book
-  
-#   pdf.text "THIS IS THE FIRST PAGE"
-#   pdf.text "PUT SOMETHING COOL HERE"
-#   pdf.start_new_page
-#   Recipe.all.each do |recipe|
-#     pdf.text recipe.name
-#     pdf.text "BY: #{recipe.user.email}"
-#     pdf.move_down 10
-#     pdf.text "Ingredients:"
-#     pdf.text recipe.ingredients
-#     pdf.move_down 10
-#     pdf.text "Instructions:"
-#     pdf.text recipe.instructions
-#     pdf.start_new_page
-#     # image_file = open(url_for(recipe.image))
-#     # pdf.image image_file.path, width: 300
-#     pdf.start_new_page
-#   end
-#   send_data pdf.render , 
-#      filename: "book", 
-#      type: "application/pdf",
-#      disposition: "inline"
-#   end
-#   end
-# end
 
 
 
