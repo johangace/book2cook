681eae12ca2c2c73240b762a12aca9e4cd8834fa
Andy Weiss <andyweiss1982@gmail.com>
2019-08-13 15:32:07 -0400

    find_by in session instead of find

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_book
 
   def current_book
-    current_user.books.find(session[:book_id]) if session[:book_id].present?
+    current_user.books.find_by(id: session[:book_id]) if session[:book_id].present?
   end
 end
