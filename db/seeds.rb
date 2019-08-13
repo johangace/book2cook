408b0b2c1c4f58461cd2849595882fd5f60b2db1
Andy Weiss <andyweiss1982@gmail.com>
2019-08-13 15:31:54 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,41 +12,53 @@ User.destroy_all
 user = User.create(email:"someguy@email.com", password:"password")
 
 #Create sandwhich recipe
-sandwhich = Recipe.create(
-  name: "sandwhich",
+sandwich = Recipe.create(
+  name: "Sandwich",
   ingredients: "Bread\r\nCheese\r\nHam",
   instructions: "Put cheese and ham on the bread",
   category: "sandwhich",
   author: "Bob",
-  user_id: 1
+  user: user
 )
+
+sandwich.image.attach(io: File.open(Rails.root.join('public', 'background.png')), filename: 'background.png')
+
 #Create banana smoothie
-bananaSmoothie = Recipe.create(
-    name: "Banana smoothie",
-    ingredients: "Banana\r\nMilk\r\nSugar",
-    instructions: "Put banana, milk, and sugar in the blender and enjoy",
-    category: "Smoothie",
-    author: "bobby",
-    user_id: 1
+banana_smoothie = Recipe.create(
+  name: "Banana smoothie",
+  ingredients: "Banana\r\nMilk\r\nSugar",
+  instructions: "Put banana, milk, and sugar in the blender and enjoy",
+  category: "Smoothie",
+  author: "bobby",
+  user: user
 )
+
+banana_smoothie.image.attach(io: File.open(Rails.root.join('public', 'background.png')), filename: 'background.png')
+
 #Create chicken recipe
 chicken = Recipe.create(
-    name: "Chicken",
-    ingredients: "Chicken\r\nOnion\r\nPowder\r\nGarlic Powder\r\nSalt Pepper",
-    instructions: "Add seasoning to chicken\r\nCook chicken",
-    category: "Poultry",
-    author: "johnny m",
-    user_id: 1
+  name: "Chicken",
+  ingredients: "Chicken\r\nOnion\r\nPowder\r\nGarlic Powder\r\nSalt Pepper",
+  instructions: "Add seasoning to chicken\r\nCook chicken",
+  category: "Poultry",
+  author: "johnny m",
+  user: user
 )
+
+chicken.image.attach(io: File.open(Rails.root.join('public', 'background.png')), filename: 'background.png')
+
 #Create mac and cheese recipe
-macAndCheese = Recipe.create(
-      name: "Mac and Cheese",
-      ingredients: "Mac\r\nCheese\r\nFlour\r\nSalt\r\nPepper",
-      instructions: "Cook Pasta\r\nBoil Milk\r\nAdd dry ingrediant and cheese",
-      category: "Pasta",
-      author: "Bob",
-      user_id: 1
+mac_and_cheese = Recipe.create(
+  name: "Mac and Cheese",
+  ingredients: "Mac\r\nCheese\r\nFlour\r\nSalt\r\nPepper",
+  instructions: "Cook Pasta\r\nBoil Milk\r\nAdd dry ingrediant and cheese",
+  category: "Pasta",
+  author: "Bob",
+  user: user
 )
+
+mac_and_cheese.image.attach(io: File.open(Rails.root.join('public', 'background.png')), filename: 'background.png')
+
 #Display in the console when create
 puts "#{Recipe.count} recipes"
 puts "#{User.count} users"
