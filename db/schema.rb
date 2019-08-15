21ea6cb02cafc94b5a60c214ca9644d6e4d67287
johan gace <gace.johan3@gmail.com>
2019-08-15 11:36:50 -0400

    added customizable fields for the book subtitle, dedication, footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_183211) do
+ActiveRecord::Schema.define(version: 2019_08_15_145917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2019_08_10_183211) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "subtitle"
+    t.text "dedication"
+    t.text "footer"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
